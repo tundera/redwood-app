93fe54a88acbb00312867290577aa2f181c43151
tundera <tunderadev@gmail.com>
2021-05-03 15:13:05 -0600

    format schema

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -19,17 +19,17 @@ const Routes = () => {
       <Route path="/players/new" page={NewPlayerPage} name="newPlayer" />
       <Route path="/players/{id}/edit" page={EditPlayerPage} name="editPlayer" />
       <Route path="/players/{id}" page={PlayerPage} name="player" />
-      <Route path="/players" page={PlayersPage} name="players" />
+      <Route path="/players" page={PlayersPage} name="players" prerender />
       <Route path="/coaches/new" page={NewCoachPage} name="newCoach" />
       <Route path="/coaches/{id}/edit" page={EditCoachPage} name="editCoach" />
-      <Route path="/coaches/{id}" page={CoachPage} name="coach" />
+      <Route path="/coaches/{id}" page={CoachPage} name="coach" prerender />
       <Route path="/coaches" page={CoachesPage} name="coaches" />
       <Route path="/teams/new" page={NewTeamPage} name="newTeam" />
       <Route path="/teams/{id}/edit" page={EditTeamPage} name="editTeam" />
       <Route path="/teams/{id}" page={TeamPage} name="team" />
-      <Route path="/teams" page={TeamsPage} name="teams" />
+      <Route path="/teams" page={TeamsPage} name="teams" prerender />
       <Route path="/about" page={AboutPage} name="about" />
-      <Route path="/" page={HomePage} name="home" />
+      <Route path="/" page={HomePage} name="home" prerender />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
