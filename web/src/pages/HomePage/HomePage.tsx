eeed03762c6e385473968c7e20657e5ef1019dce
tundera <tunderadev@gmail.com>
2021-05-14 13:28:11 -0600

    upgrade to v0.32.2

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,22 +1,24 @@
 import { Link as RedwoodLink, routes } from '@redwoodjs/router'
 import { Link, Text } from '@chakra-ui/layout'
-import { CallToAction } from '@monorepo/components'
+// import { CallToAction } from '@monorepo/components'
 
 import CenterCta from 'src/components/CenterCta/CenterCta'
 import ColumnFeatures from 'src/components/ColumnFeatures/ColumnFeatures'
 import PriceTiers from 'src/components/PriceTiers/PriceTiers'
+import DataTable from 'src/components/DataTable/DataTable'
 
 function HomePage() {
   return (
     <>
       <CenterCta />
+      <DataTable />
       <ColumnFeatures />
       <PriceTiers />
-      <CallToAction
+      {/* <CallToAction
         title="Title"
         subtitle="subtitle"
         description="Some more text"
-      />
+      /> */}
       <Text>
         My default route is named
         <code>home</code>, link to me with `
