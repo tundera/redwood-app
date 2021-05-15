fe8db333f9f16dc955ed6fa821b692b45e3a66fb
tundera <tunderadev@gmail.com>
2021-05-15 12:39:56 -0600

    done

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -1,20 +1,21 @@
 import {
-	createGraphQLHandler,
-	makeMergedSchema,
-	makeServices,
-} from "@redwoodjs/api";
+  createGraphQLHandler,
+  makeMergedSchema,
+  makeServices,
+} from '@redwoodjs/api'
 
-import schemas from "src/graphql/**/*.{js,ts}";
-import {db} from "src/lib/db";
-import services from "src/services/**/*.{js,ts}";
+import schemas from 'src/graphql/**/*.{js,ts}'
+import { db } from 'src/lib/db'
+import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
-	schema: makeMergedSchema({
-		schemas,
-		services: makeServices({services}),
-	}),
-	onException: () => {
-		// Disconnect from your database with an unhandled exception.
-		db.$disconnect();
-	},
-});
+  schema: makeMergedSchema({
+    schemas,
+    services: makeServices({ services }),
+  }),
+  onException: () => {
+    // Disconnect from your database with an unhandled exception.
+    db.$disconnect()
+  },
+  introspection: true,
+})
